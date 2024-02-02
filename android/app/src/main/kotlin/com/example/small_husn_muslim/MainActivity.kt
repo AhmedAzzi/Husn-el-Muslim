c053397569fa58b3306bd31699d4458cdbcbc118
AhmedAzzi <mrahmedazzi@gmail.com>
2024-02-02 20:35:08 +0100

    husn el muslim 1st version

---
+++ android/app/src/main/kotlin/com/example/small_husn_muslim/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.small_husn_muslim
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constant/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color bgLight = const Color.fromARGB(255, 255, 252, 254);
+Color bgDark = const Color.fromARGB(255, 39, 34, 43);
