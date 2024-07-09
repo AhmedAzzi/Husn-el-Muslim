cc628524ec29d586a0ed268d82c11837d7f5df39
AhmedAzzi <mrahmedazzi@gmail.com>
2024-07-09 16:27:03 +0100

    some otimization for husn muslim app

---
+++ android/app/src/main/kotlin/com/example/husn_el_muslim/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.husn_el_muslim
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/azkar_class.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AzkarInfo {
+  final int id;
+  final String category;
+  final String audio;
+  final String filename;
+  final List<AzkarArrayInfo> array;
+  final List<String> footnote;
+
+  AzkarInfo({
+    required this.id,
+    required this.category,
+    required this.audio,
+    required this.filename,
+    required this.array,
+    required this.footnote,
+  });
+
+  factory AzkarInfo.fromJson(Map<String, dynamic> json) {
+    return AzkarInfo(
+      id: json['id'],
+      category: json['category'],
+      audio: json['audio'],
+      filename: json['filename'],
+      array: (json['array'] as List)
+          .map((arrayItem) => AzkarArrayInfo.fromJson(arrayItem))
+          .toList(),
+      footnote: List<String>.from(json['footnote']),
+    );
+  }
+}
+
+class AzkarArrayInfo {
+  final int id;
+  final String text;
+  int count;
+  final int fix;
+  final String audio;
+  final String filename;
+
+  AzkarArrayInfo({
+    required this.id,
+    required this.text,
+    required this.count,
+    required this.fix,
+    required this.audio,
+    required this.filename,
+  });
+
+  factory AzkarArrayInfo.fromJson(Map<String, dynamic> json) {
+    return AzkarArrayInfo(
+      id: json['id'],
+      text: json['text'],
+      count: json['count'],
+      fix: json['count'],
+      audio: json['audio'],
+      filename: json['filename'],
+    );
+  }
+}
+
+Future<String> loadAzkarJson() async {
+  return await rootBundle.loadString('assets/hisnmuslim.json');
+}
+
+Future<void> openURL(String url) async {
+  final customURL = url;
+  final parsed = Uri.parse(customURL);
+  try {
+    if (await canLaunchUrl(parsed)) {
+      await launchUrl(parsed);
+    }
+  } catch (_) {}
+}
+
+setupAudioPlayer(AudioPlayer player, String url) async {
+  player.playbackEventStream.listen((event) {},
+      onError: (Object e, StackTrace stacktrace) {
+    if (kDebugMode) {
+      print("A stream error occurred: $e");
+    }
+  });
+  try {
+    await player.setAudioSource(AudioSource.uri(Uri.parse(url)));
+  } catch (e) {
+    if (kDebugMode) {
+      print("Error loading audio source: $e");
+    }
+  }
+}
